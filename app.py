fb21b41178784cea182df3219b16d6dbfda7df7f
Igor Santos <104454066+IgorLopes08@users.noreply.github.com>
2024-11-08 15:14:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ ax.set_ylabel('Frequência')
 plt.xticks(rotation=45)
 
 # Exibindo o gráfico no Streamlit
-st.image('data\\image.png', use_column_width=True)
+#st.image('data\\image.png', use_column_width=True)
 
 
 st.write(f"<h5 style='color:white;'>Aplicando o vader sentiment dentro do banco de dados e categorizando a polaridade das classes desejadas, tivemos um resultado esperado de que a maioria das reviews seriam positivas, com as classes muito bom, bom e médio ultrapassando muito as avaliações neutras e negativas</h5>", unsafe_allow_html=True)
